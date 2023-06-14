554dd27e338f21dda6a958244828bfeab46b5502
ekeoma1 <ekeigwee@gmail.com>
2023-06-14 04:07:59 +0100

    about page updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,13 +12,13 @@ const About = () => {
     <div className="about-page py-4">
       <div className="container">
         <h2>About Us</h2>
-        <div className="row mt-4">
-          <div className="col position-relative img-wrapper">
-            <img width="250" src={Img1} alt="" />
-            <img width="150" src={Card1} alt="" />
-            <img width="150" src={Card2} alt="" />
+        <div className="row mt-4 align-items-center ">
+          <div className="col-6 position-relative img-wrapper">
+            <img width="300" src={Img1} alt="" />
+            <img width="200" src={Card1} alt="" />
+            <img src={Card2} alt="" />
           </div>
-          <div className="col">
+          <div className="col position-relative">
             <div className="statement-details">
               We are building what financial services should look like for the next generation of immigrants. A financial service app that understands
               you and cares for you.
@@ -27,11 +27,8 @@ const About = () => {
               <div className="header">Mission Statement</div>
               We are on a mission to provide financial services that enables every business and consumer realize their full potential.
             </div>
+            <img className="robot-img" width="150" src={Robot} alt="" />
           </div>
-        </div>
-
-        <div className="text-end">
-          <img width="150" src={Robot} alt="" />
         </div>
       </div>
 
