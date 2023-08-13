c05c6f26f05f0c67e96e5604541eb144cc6ba0da
Oke Babatunde <tundejosilnet@gmail.com>
2023-08-13 07:37:04 +0100

    our links

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -11,12 +11,22 @@ const Newsletter = () => {
   return (
     <section className="py-4 newsletter-section">
       <div className="container">
-        <h4 className="fw-bold">Stay updated with Bitscard by signing up for our newsletter</h4>
+        <h4 className="fw-bold">
+          Stay updated with Bitscard by signing up for our newsletter
+        </h4>
         <div className="row gap-lg-0 gap-4">
           <div className="col-lg">
             <form className="d-flex newsletter-form">
-              <input className="newsletter-input" type="text" placeholder="Enter your email" />
-              <input className="newsletter-btn" type="submit" value="Subscribe" />
+              <input
+                className="newsletter-input"
+                type="text"
+                placeholder="Enter your email"
+              />
+              <input
+                className="newsletter-btn"
+                type="submit"
+                value="Subscribe"
+              />
             </form>
           </div>
           <div className="col-lg">
@@ -24,21 +34,27 @@ const Newsletter = () => {
               <a target="_blank" href="https://www.t.me/bitscardcomm">
                 <img src={Tumblr} alt="tumblr" />
               </a>
-              <a target="_blank" href="https://www.linkedin.com/company/bitscard-co/">
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/company/bitscard-co/"
+              >
                 <img src={Linkedin} alt="linkedin" />
               </a>
               <a target="_blank" href="http://Facebook.com/bitscardng">
                 <img src={Fb} alt="facebook" />
               </a>
-              <a target="_blank" href="#">
-                <img src={Insta} alt="http://instagram.com/bitscardng" />
+              <a target="_blank" href="http://instagram.com/bitscardng">
+                <img src={Insta} alt="instagram" />
               </a>
               <a target="_blank" href="http://Twitter.com/bitscardng">
                 <img src={Twitter} alt="Twitter" />
               </a>
             </div>
             <div className="app-btn-wrapper ms-auto">
-              <a target="_blank" href="https://play.google.com/store/apps/details?id=com.val.bitscard">
+              <a
+                target="_blank"
+                href="https://play.google.com/store/apps/details?id=com.val.bitscard"
+              >
                 <div className="app-btn">
                   <img src={playStoreLogo} alt="google play logo" />
                   <span>Google play</span>
