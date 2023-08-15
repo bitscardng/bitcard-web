c389074bb5ca7154930a9acd97ad0528630666b9
Oke Babatunde <tundejosilnet@gmail.com>
2023-08-15 23:15:44 +0100

    blog section

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -10,7 +10,9 @@ const Blog = () => {
     try {
       const {
         data: { data },
-      } = await axios.get(`http://ec2-3-231-77-121.compute-1.amazonaws.com:3000/api/v1/news?limit=3`);
+      } = await axios.get(
+        `http://ec2-3-231-77-121.compute-1.amazonaws.com:3000/api/v1/news?limit=3`
+      );
       setBlog(data);
       console.log(data);
     } catch (error) {
@@ -24,7 +26,11 @@ const Blog = () => {
     <div className="blog-section">
       <div className="container">
         <div className="text-center mt-4 mb-5">
-          <Link style={{ background: "#FCB721" }} className="btn-without-shadow px-5" to="/blog">
+          <Link
+            style={{ background: "#FCB721" }}
+            className="btn-without-shadow px-5"
+            to="/blog"
+          >
             Blogs
           </Link>
         </div>
@@ -36,11 +42,14 @@ const Blog = () => {
             ? "loading..."
             : blog.map((blog) => {
                 return (
-                  <div key={blog._id} className="col">
+                  <div key={blog._id} className="co" style={{ flex: "row" }}>
                     <Link to={`/blog/${blog._id}`}>
                       <div className="card blog-card mx-auto border-0 shadow">
                         <img className="card-img-top" src={blog.image} alt="" />
-                        <div style={{ background: "#FCB721" }} className="card-body text-center">
+                        <div
+                          style={{ background: "#FCB721" }}
+                          className="card-body text-center"
+                        >
                           {blog.title}
                         </div>
                       </div>
